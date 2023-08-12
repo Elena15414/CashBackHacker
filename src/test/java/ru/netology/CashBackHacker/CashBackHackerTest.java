8f15788a05079873548f3b5d1cfa6ade5dc987d2
unknown <you@example.com>
2023-08-12 00:30:32 -0700

    testng

---
+++ src/test/java/ru/netology/CashBackHacker/CashBackHackerTest.java
@@ -0,0 +1,38 @@
+package ru.netology.CashBackHacker;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import static org.testng.Assert.*;
+
+public class CashBackHackerTest {
+
+    @Test
+    public void testRemain() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+
+        int actual = cashBackHacker.remain(900);
+        int expected = 100;
+
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testRemainZero() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+
+        int actual = cashBackHacker.remain(0);
+        int expected = 1000;
+
+        assertEquals(actual,expected);
+    }
+    @Test
+    public void testRemainMax() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+
+        int actual = cashBackHacker.remain(1000);
+        int expected = 1000;
+
+        assertEquals(actual,expected);
+    }
+}
