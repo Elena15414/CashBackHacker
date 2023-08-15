2e8c5526f1c19748011cebe9db5017f53b4c05a4
unknown <you@example.com>
2023-08-14 23:13:24 -0700

    junit4-platform

--- src/test/java/ru/netology/CashBackHacker/CashBackHackerTest.java
+++ src/test/java/ru/netology/CashBackHacker/CashBackHackerTest.java
@@ -36,4 +36,33 @@ public class CashBackHackerTest {
 
         assertEquals(expected, actual);
     }
+    @org.junit.jupiter.api.Test
+    public void testRemainJunitJupiter() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+
+        int actual = cashBackHacker.remain(900);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainZeroJunitJupiter() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+
+        int actual = cashBackHacker.remain(0);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemainMaxJunitJupiter() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+
+        int actual = cashBackHacker.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }
